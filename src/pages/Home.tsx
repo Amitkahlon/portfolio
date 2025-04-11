cd5951567f58831a416370337289600e68a4a064
Amit Kahlon <amitkahlon4321@gmail.com>
2025-04-11 06:20:07 +0300

    wip

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import projects from "../projects.json"
+
 const Home = () => {
+    const recommendedProjects = projects;
+
     return <div id="HomeBody">
         <h1 id="HomeHeader">My portfolio</h1>
         <p id="HomeDesc">This is my portfolio, I recommend you check out this projects out</p>
+        {recommendedProjects.map(p => {
+            return <Link to={"/project/" + p.id}>
+                <h4>{p.name}</h4>
+                <img src=""/>
+            </Link>
+        })}
     </div>
 }
 
